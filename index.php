ed8f0a995abcf78af29fcf314941c7fb3d7151fd
creativecbr <lesniakcbr@gmail.com>
2017-03-13 17:34:30 +0100

    Główna strona
    Robiłem to godzine, bo jeszcze z polaka trzeba sie uczyć, mówcie co do
    poprawy, to jest wersja 1.0 :+1:

--- index.php
+++ index.php
@@ -2,9 +2,46 @@
 <html>
 <head>
 	
-	<link href="prism.css" rel="stylesheet" />
+	<link href="style.css" rel="stylesheet" />
+	<link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet">
 </head>
 <body>
-	<p> elo </p>
+	<div id="content">
+		<div id="up">
+			<div id="up-left">
+				<input value="Wyszukaj.." id="up-left-form" type="text" />
+			</div>
+			
+			<div id="up-right">
+				<div class="icon-social" id="icon1"><img src="http://www.plumedecrivain.com/wp-content/uploads/2016/05/facebook-150x150.png" width="50px" height="50px"/> </div>
+				<div class="icon-social" id="icon2"><img src="http://www.plumedecrivain.com/wp-content/uploads/2016/05/facebook-150x150.png" width="50px" height="50px"/> </div>
+				<div class="icon-social" id="icon3"><img src="http://www.plumedecrivain.com/wp-content/uploads/2016/05/facebook-150x150.png" width="50px" height="50px"/> </div>
+			
+			</div>
+		</div>
+		<div id="content-logo">
+		
+			<img src="http://morethangamers.com/landingpage2/img/companies/growup.png"  height="250px" width="auto"/>  	
+		</div>
+		
+		<div id="content-comment">		
+			<h1>Możesz nauczyć się wszystkiego. <p>Za darmo. Dla każdego. Na zawsze.</p></h1>
+		</div>
+		
+		<div id="bottom">
+		
+			<div id="bottom-left">
+				Masz konto? <div id="bottom-radius">Zaloguj się!</div>
+			</div>
+			<div id="bottom-right">
+			
+				Nie masz konta?  <div id="bottom-radius">Zajerejestruj się! </div>
+			</div>
+		
+		</div>
+		
+		<div id="footer"></div>
+	
+	</div>
 </body>
 </html>
