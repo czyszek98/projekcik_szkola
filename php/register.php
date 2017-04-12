756da2df7937e5004a615eea387448dd5ab2bca5
creativecbr <lesniakcbr@gmail.com>
2017-04-12 14:26:40 +0200

    jakies stare aktualizacje znakow pl

--- php/register.php
+++ php/register.php
@@ -1,4 +1,4 @@
-<?php
+ï»¿<?php
 
 include "connect.php";
 
@@ -17,11 +17,11 @@ if($result->num_rows > 0)
     $row=$result->fetch_assoc();
     if($row["login"] == $obj->login)
     {
-        echo '{"code":-1,"error":"U¿ytkownik o podanym loginie ju¿ istnieje!"}';
+        echo '{"code":-1,"error":"UÅ¼ytkownik o podanym loginie juÅ¼ istnieje!"}';
     }
    elseif ($row["email"] == $obj->email)
    {
-         echo '{"code":-1,"error":"Podany adress email jest ju¿ zajêty!"}';
+         echo '{"code":-1,"error":"Podany adress email jest juÅ¼ zajÄ™ty!"}';
          
    }
 }
@@ -30,8 +30,8 @@ else
 $query="INSERT INTO `users`(`login`, `password`, `email`, `account_type`) VALUES ('$obj->login','$obj->password','$obj->email','$obj->account_type')";    
 
 $result = $mysql->query($query);
-
-$response->url="http://localhost/projekcik_szkola/sign-in.html";
+	
+$response->url="http://localhost/projekcik_szkola/php/user-log-in.php";
 $response->code=200;
 
 echo json_encode($response);
